60a9b3e8a28b7f4f7d6590e68800e363b5e4af57
Jarrod Watts <jarrodwattsyt@gmail.com>
2024-08-13 11:54:51 +1000

    get the extension to work?

---
+++ src/contracts/MyPaymaster.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IPaymaster, ExecutionResult, PAYMASTER_VALIDATION_SUCCESS_MAGIC} from "@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IPaymaster.sol";
+
+contract MyPaymaster is IPaymaster {}
